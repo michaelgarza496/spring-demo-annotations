195321f6a316db69796c9aa2486792dfa9699d95
Michael Garza <michaelgarza496@gmail.com>
2019-01-21 12:35:43 -0500

    default component test

--- src/com/michael/springdemo/TennisCoach.java
+++ src/com/michael/springdemo/TennisCoach.java
@@ -1,8 +1,11 @@
 package com.michael.springdemo;
 
 import org.springframework.stereotype.Component;
+// if no name is given for the bean id, then the default will be used
+// @Component will have the default bean of @Component("tennisCoach")
+// camel case is required
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
